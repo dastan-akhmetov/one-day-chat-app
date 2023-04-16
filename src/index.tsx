5329f92464d12bdd3f7b49f297f4cfe8b10dc931
Dastan Akhmetov <ddakhmet@gmail.com>
2023-04-16 19:13:58 +0600

    Make an app

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MantineProvider } from "@mantine/core";
+import { UserStoreContextProvider } from "./app/store/user";
+import { ChannelStoreContextProvider } from "./app/store/channel";
+import { MessageStoreContextProvider } from "./app/store/message";
+import { TextStoreContextProvider } from "./app/store/text";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserStoreContextProvider>
+      <ChannelStoreContextProvider>
+        <TextStoreContextProvider>
+          <MessageStoreContextProvider>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+              <App />
+            </MantineProvider>
+          </MessageStoreContextProvider>
+        </TextStoreContextProvider>
+      </ChannelStoreContextProvider>
+    </UserStoreContextProvider>
   </React.StrictMode>
 );
 
